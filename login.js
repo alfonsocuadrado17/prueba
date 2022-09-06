7b65b09560f78254a6e97473b9043b4f90b666ec
alfonsocuadrado17 <112915976+alfonsocuadrado17@users.noreply.github.com>
2022-09-06 00:25:44 -0300

    Intento login

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-/* function googleLogin() {
+function googleRedirect() {
     if (googleUser.getBasicProfile()) {
         let profile = googleUser.getBasicProfile();
         localStorage.setItem('User', profile.getEmail());
@@ -6,4 +6,8 @@
     }
 }
 
- */
+let profile = googleUser.getBasicProfile();
+
+document.getElementById("texto").innerHTML = profile.getEmail();
+
+localStorage.setItem("User", profile.getEmail());
